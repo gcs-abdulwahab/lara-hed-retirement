2d258a1ae0bde1f7c4c7f98c84b48c2eafde7df5
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2023-10-01 13:02:56 +0500

    filament installed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+
+        User::factory()->create(
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin@admin.com'),
+
+            ]
+        );
+
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
