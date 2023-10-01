48ccb8a261da02d39560765d7604777a9b34979e
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2023-10-01 16:44:34 +0500

    customer and Designations test

---
+++ app/Filament/Resources/DesignationResource.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DesignationResource\Pages;
+use App\Filament\Resources\DesignationResource\RelationManagers;
+use App\Models\Designation;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DesignationResource extends Resource
+{
+    protected static ?string $model = Designation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageDesignations::route('/'),
+        ];
+    }    
+}
---
+++ app/Filament/Resources/DesignationResource/Pages/ManageDesignations.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\DesignationResource\Pages;
+
+use App\Filament\Resources\DesignationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageDesignations extends ManageRecords
+{
+    protected static string $resource = DesignationResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
